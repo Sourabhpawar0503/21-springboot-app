0fc953274b51258a6020dbab081366b3b37fd619
Sourabhpawar0503 <sourabhpawar0503@gmail.com>
2023-12-19 19:12:19 +0530

    Previous task

--- src/Third3.java
+++ src/Third3.java
@@ -2,6 +2,10 @@
 public class Third3 {
 public static void main(String[] args) {
 	System.out.println("Third Class");
+<<<<<<< Updated upstream
 	System.out.println("Changes for another class");
+=======
+	System.out.println("Continue..");
+>>>>>>> Stashed changes
 }
 }
